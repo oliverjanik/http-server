50dda8ef6c6c6bc8684f3b639bc4639ba98e3991
Oliver <oliver.janik@gmail.com>
2014-04-02 23:39:02 +1100

    ResponseWriter wrap to log status code.

--- file-server.go
+++ file-server.go
@@ -7,13 +7,21 @@ import (
 	"os"
 )
 
-type loggingHandler struct {
-	h http.Handler
+type loggingResponseWriter struct {
+	http.ResponseWriter
 }
 
-func (f *loggingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%s %s", r.Method, r.URL)
-	f.h.ServeHTTP(w, r)
+func (w *loggingResponseWriter) WriteHeader(code int) {
+	log.Printf("%d", code)
+	w.ResponseWriter.WriteHeader(code)
+}
+
+func wrapHandler(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		lw := &loggingResponseWriter{w}
+		log.Printf("%s %s", r.Method, r.URL)
+		handler.ServeHTTP(lw, r)
+	})
 }
 
 func main() {
@@ -22,13 +30,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	lh := &loggingHandler{http.FileServer(http.Dir(wd))}
-
 	port := 8001
 
 	log.Printf("Starting server on port %d", port)
 
 	addr := fmt.Sprintf(":%d", port)
 
-	log.Fatal(http.ListenAndServe(addr, lh))
+	log.Fatal(http.ListenAndServe(addr, wrapHandler(http.FileServer(http.Dir(wd)))))
 }
